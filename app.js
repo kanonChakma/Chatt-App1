7f28c20615bab50de74b6d4addd1d843e4e32acf
kanon chakma <kanonchakma@pop-os.localdomain>
2022-09-23 11:55:29 +0600

    feat: add error handler

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+
+const {notFoundHadnler, errorHandler} = require('./middleware/common/errorHandler.js')
+
+
+const app = express();
+dotenv.config();
+
+
+//connect database
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("database connected"))
+.catch((err) => console.log(err));
+
+//request parser
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+
+//set view engine
+app.set("view engine", "ejs");
+
+//set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//parse cookie
+app.use(cookieParser(process.env.COOKIE_SECRET))
+
+//routing setup
+
+//404 not found handler
+app.use(notFoundHadnler);
+
+//common error handler
+app.use(errorHandler);
+
+app.listen(process.env.PORT, () => {
+    console.log(`app is listening to port ${process.env.PORT}`)
+})
