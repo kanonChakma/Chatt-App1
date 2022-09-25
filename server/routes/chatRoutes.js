f15df0a6b918e7588b26e350ec7b93a2aaaba63d
kanon chakma <kanonchakma@pop-os.localdomain>
2022-09-25 18:59:53 +0600

    add createGroup chat

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,12 +1,10 @@
 import express from 'express';
-import { accessChat, fetchChats } from '../controllers/chattControllers.js';
+import { accessChat, fetchChats, groupChat } from '../controllers/chattControllers.js';
 import { authCheck } from '../middleware/authMiddleware.js';
 
-const router = express.Router(
-    
-)
+const router = express.Router()
 router.route("/").post(authCheck, accessChat).get(authCheck,fetchChats)
-
+router.post("/group", authCheck, groupChat)
 
 
 export default router;
