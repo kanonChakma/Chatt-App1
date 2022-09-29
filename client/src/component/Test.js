b5b5954a915fc6d01ddb5b8e9af4bde6964988ec
kanon chakma <kanonchakma@pop-os.localdomain>
2022-09-29 09:56:19 +0600

    feat: add responsive

--- client/src/component/Test.js
+++ client/src/component/Test.js
@@ -1,14 +1,16 @@
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Button, Flex } from "@chakra-ui/react";
 
 const Test = () => {
  return (
+   <>
+
      <Box
      pb={3}
      px={3}
      fontSize={{ base: "28px", md: "30px" }}
      fontFamily="Work sans"
-     d="flex"
-     w="100%"
+     display="flex"
+     width="100%"
      justifyContent="space-between"
      alignItems="center"
      >
@@ -17,7 +19,27 @@ const Test = () => {
          hello
        </Button>
      </Box>
- )
+    <Flex
+    pb={3}
+    px={3}
+    fontSize={{ base: "28px", md: "30px" }}
+    fontFamily="Work sans"
+    d="flex"
+    w="100%"
+    justifyContent="space-between"
+    alignItems="center"
+    >
+          
+    <p>Fisrt</p>
+    <Box display ={{ base:"none", md: "flex" }}>
+      <p>second</p>
+    </Box>
+    <Button>
+      hello
+    </Button>
+    </Flex>
+     </>
+     )
 }
 
 export default Test;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -18,11 +18,11 @@ const Home = () =>{
  return(
     <Container maxW="xl" centerContent>
     <Box
-      d="flex"
+      display="flex"
       justifyContent="center"
       p={3}
       bg="white"
-      w="100%"
+      width="100%"
       m="40px 0 15px 0"
       borderRadius="lg"
       borderWidth="1px"
